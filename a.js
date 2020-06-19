0fc56df23bcc9fc421fb9487f85a81fd9fef87e7
phamtanphat <phatdroid94@gmail.com>
2020-06-19 05:29:14 -0700

    init

---
+++ a.js
@@ -0,0 +1,26 @@
+// let a = 5
+// getData((a) => console.log(a))
+
+// function getData(cb){
+//     setTimeout(() => {
+//         a = a + 1
+//         cb(a)
+//     } , 1000)
+// }
+const request = require('request');
+// npm i request
+
+
+function getTempCity(cityName , cb){
+    const URL = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityName}`
+
+    request(URL ,{json : true} ,function(error ,response , body){
+        if (error) return cb(error.message)
+        if (body.cod != 200) return cb(body.message)
+        cb (null,body.main.temp)
+    })
+}
+
+getTempCity("Lodon",function(error , response){
+    console.log(error || response)
+})
