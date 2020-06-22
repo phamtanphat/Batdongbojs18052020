88dcb9957c823a51c19b3dc6767f49c8be82a8f9
phamtanphat <phatdroid94@gmail.com>
2020-06-22 05:33:21 -0700

    generate function

--- c.js
+++ c.js
@@ -1,37 +1,48 @@
-const request = require('request');
+// const request = require('request');
 
 
-function cong(a , b ){
-    return new Promise((resolve , reject) => {
-        const url = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}` 
+// function cong(a , b ){
+//     return new Promise((resolve , reject) => {
+//         const url = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}` 
 
-        request(url ,{json : true} ,function(error ,response , body){
-            if (error) return reject(error.message)
-            if (!body.success) return reject(body.message + " Cong")
-            resolve(body.message)
-        })
-    })
-}
-function tru(a , b ){
-    return new Promise((resolve , reject) => {
-        const url = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}` 
+//         request(url ,{json : true} ,function(error ,response , body){
+//             if (error) return reject(error.message)
+//             if (!body.success) return reject(body.message + " Cong")
+//             resolve(body.message)
+//         })
+//     })
+// }
+// function tru(a , b ){
+//     return new Promise((resolve , reject) => {
+//         const url = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}` 
 
-        request(url ,{json : true} ,function(error ,response , body){
-            if (error) return reject(error.message)
-            if (!body.success) return reject(body.message )
-            resolve (body.message)
-        })
-    })
-}
+//         request(url ,{json : true} ,function(error ,response , body){
+//             if (error) return reject(error.message)
+//             if (!body.success) return reject(body.message )
+//             resolve (body.message)
+//         })
+//     })
+// }
 
-async function xuly(){
-    try {
-        const tong = await cong(5 , null)
-        const hieu = await tru(tong , 1)
-        console.log(hieu)
-    } catch (error) {
-        console.log(error)
-    } 
-}
+// async function xuly(){
+//     try {
+//         const tong = await cong(5 , null)
+//         const hieu = await tru(tong , 1)
+//         console.log(hieu)
+//     } catch (error) {
+//         console.log(error)
+//     } 
+// }
 
-xuly()
+// xuly()
+
+function* generatorFunc(index) {
+    while (index < 2) {
+      yield index++;
+    }
+  }
+  
+const iterator = generatorFunc(0);
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
